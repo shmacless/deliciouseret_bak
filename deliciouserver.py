420f7fa9fcdcbdbe49fa786301ed1745f5b9d934
omrikap <omrikap@gmail.com>
2015-11-25 21:27:46 +0200

    Collector open by default

--- deliciouserver.py
+++ deliciouserver.py
@@ -7,9 +7,15 @@ class MainHandler(tornado.web.RequestHandler):
         self.write("DeliciouSeret")
 
 
+class Collector(tornado.web.RequestHandler):
+    def get(self):
+        # items = ["Item 1", "Item 2", "Item 3"]
+        self.render("front/html/collector.html")  # , title="My title", items=items)
+
+
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/", Collector),
     ])
 
 
