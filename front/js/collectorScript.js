6baea4a87830685f966b9bba192149804ff0729d
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2015-11-25 20:52:17 +0200

    collector

---
+++ front/js/collectorScript.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Erez Levanon on 25/11/2015.
+ */
+reloadPage();
+
+function reloadPage()
+{
+    info = getInfo();
+    document.getElementById("movieName").innerHTML='HollyMolly';
+    document.getElementById("foodName").innerHTML='guakamolly';
+    document.getElementById("moviePic").src = "https://static.pexels.com/photos/7976/pexels-photo.jpg";
+    document.getElementById("foodPic").src = "https://static.pexels.com/photos/7976/pexels-photo.jpg";
+}
+
+function sendRate()
+{
+
+}
+
+function getInfo()
+{
+
+}
